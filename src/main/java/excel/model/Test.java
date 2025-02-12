528adb9d693978c817b2a9a555e0cd1d0249054b
Hamza Stitou <hamzastitou11s@hotmail.com>
2025-02-12 14:17:31 +0100

    feat(model) : ISEMPTY()

--- src/main/java/excel/model/Test.java
+++ src/main/java/excel/model/Test.java
@@ -2,7 +2,7 @@ package excel.model;
 
 public class Test {
     public static void main(String[] args) {
-        String a = "= 6       - 3";
+        String a = "=    3       - 6";
         ExpressionBuilder builder = new ExpressionBuilder();
         Expression ex1 = builder.build(a);
         System.out.println(ex1.interpret());
