e3f1e6b29a939fc939f539c5d86fe1cc9cee382f
Hamza Stitou <hamzastitou11s@hotmail.com>
2025-02-17 08:34:57 +0100

    builder pour * et /

--- src/main/java/excel/model/Test.java
+++ src/main/java/excel/model/Test.java
@@ -2,9 +2,9 @@ package excel.model;
 
 public class Test {
     public static void main(String[] args) {
-        String a = "=    3       - 6";
+        String a = "=5*2>5+3";
         ExpressionBuilder builder = new ExpressionBuilder();
         Expression ex1 = builder.build(a);
-        System.out.println(ex1.interpret());
+         System.out.println(ex1.interpret());
     }
 }
