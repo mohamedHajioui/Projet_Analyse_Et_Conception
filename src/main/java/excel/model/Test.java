d51abc9402add383279c010a3d3ccce6577df8b4
Mohamed El Hajioui <mohamedelhajioui3@gmail.com>
2025-02-17 08:25:33 +0100

    test

--- src/main/java/excel/model/Test.java
+++ src/main/java/excel/model/Test.java
@@ -1,8 +1,11 @@
 package excel.model;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        String a = "= 6       - 3";
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
         ExpressionBuilder builder = new ExpressionBuilder();
         Expression ex1 = builder.build(a);
         System.out.println(ex1.interpret());
