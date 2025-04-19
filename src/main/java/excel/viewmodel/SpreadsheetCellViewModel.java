2bcfb96c89fed7fc98d9b2179e6614895f099b94
Hamza Stitou <hamzastitou11s@hotmail.com>
2025-04-19 17:11:18 +0200

    // Ne créer une commande que si la nouvelle formule est différente de l'ancienne

--- src/main/java/excel/viewmodel/SpreadsheetCellViewModel.java
+++ src/main/java/excel/viewmodel/SpreadsheetCellViewModel.java
@@ -87,7 +87,7 @@ public class SpreadsheetCellViewModel {
     public void executecomand(String formula) {
         // Ne créer une commande que si la nouvelle formule est différente de l'ancienne
         String currentFormula = model.getFormulaProperty();
-        if (!formula.equals(currentFormula)) {  // Ajoutez cette vérification
+        if (!formula.equals(currentFormula)) {
             System.out.println("Executing command avec formula: " + formula);
             Command command = new CommandManager(model, formula);
             spreadsheetModel.executeCommand(command);
